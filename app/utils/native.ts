d31b3faa69e42a920be3ce78bcd6b67d333676ef
Get <getdna99@gmail.com>
2025-10-25 11:46:08 +0700

    feat: add storage feature

--- app/utils/native.ts
+++ app/utils/native.ts
@@ -6,14 +6,15 @@ interface WebView2 {
     postMessage: (message: any) => void;
 }
 const webview: WebView2 | undefined = import.meta.client ? (window as any).chrome?.webview : undefined
-const nativeLowLevelAPIMessager = webview?.postMessage as <T extends API<string>>(api: T) => void
-interface API<APIName extends string> {
+const nativeLowLevelAPIMessager = webview?.postMessage as <T extends API<NativeFeatures>>(api: T) => void
+type NativeFeatures = 'titlebar.setDragRegion' | 'features.isAvaliable' | 'navigation.edge.flags' | 'storage.keyval' | 'storage.keyval.get' | 'storage.keyval.store'
+interface API<APIName extends NativeFeatures> {
     $api: APIName
 }
 const isNativeAvaliable = nativeLowLevelAPIMessager !== undefined
 type UUID = ReturnType<typeof crypto.randomUUID>
 const _nativeCalls: { [key in UUID]: [(value: any | PromiseLike<any>) => void, (reason?: any) => void] } = {}
-function nativeCall<T>($api: string, args: any): Promise<T> {
+function nativeCall<T>($api: NativeFeatures, args: any): Promise<T> {
     if (!import.meta.client) {
         return Promise.reject('Native API cannot be called on server.')
     }
@@ -30,7 +31,7 @@ function nativeCall<T>($api: string, args: any): Promise<T> {
         _nativeCalls[$request] = [resolve, reject]
     })
 }
-function nativeCallNoResult($api: string, args: any) {
+function nativeCallNoResult($api: NativeFeatures, args: any) {
     if (!import.meta.client) {
         return Promise.reject('Native API cannot be called on server.')
     }
@@ -71,20 +72,59 @@ const TitleBar = {
 } as const
 
 const Features = {
-    async isAvaliable(feature : string) {
-        try {
-            return await nativeCall('features.isAvaliable', { feature })
-        } catch {
-            return false
+    async isAvaliable(feature : NativeFeatures) {
+        let isAvaliable = this.$avaliableFeatures.get(feature)
+        if (isAvaliable !== undefined) {
+            return isAvaliable
         }
-    }
+        try {
+            isAvaliable = await nativeCall('features.isAvaliable', { feature }) as boolean
+        } catch {
+            isAvaliable = false
+        }
+        this.$avaliableFeatures.set(feature, isAvaliable)
+        return isAvaliable
+    },
+    $avaliableFeatures: new Map<NativeFeatures, boolean>(),
 } as const
 const FileSystem = {
-    get isAvaliable() {
-        return isNativeAvaliable
+    isAvaliable() {
+        return Promise.resolve(false)
     },
     setDragRegion(dragregion: [number, number, number, number][], passthrough: [number, number, number, number][]) {
         nativeCallNoResult("titlebar.setDragRegion", { dragregion, passthrough });
+    }
+} as const
+const KeyValueStorage = {
+    get isAvaliable() {
+        return !import.meta.client
+    },
+    isAvaliableAsNativeAPI() {
+        return Features.isAvaliable('storage.keyval')
+    },
+    async getValueAsStringAsync(key: string) : Promise<string | null> {
+        if (await this.isAvaliableAsNativeAPI())
+            return nativeCall<string>("storage.keyval.get", { key });
+        else
+            return localStorage.getItem(key);
+    },
+    async storeValueAsStringAsync(key: string, value: string) : Promise<void> {
+        if (await this.isAvaliableAsNativeAPI())
+            return nativeCall<void>("storage.keyval.store", { key, value });
+        else
+            localStorage.setItem(key, value);
+    },
+    async getValueAsync<T>(key: string) : Promise<T | null> {
+        if (await this.isAvaliableAsNativeAPI())
+            return nativeCall<T>("storage.keyval.get", { key });
+        else
+            return JSON.parse(localStorage.getItem(key) ?? 'null') as T | null;
+    },
+    async storeValueAsync<T>(key: string, value: T) : Promise<void> {
+        if (await this.isAvaliableAsNativeAPI())
+            return nativeCall<void>("storage.keyval.store", { key, value });
+        else
+            localStorage.setItem(key, JSON.stringify(value));
     }
 } as const
 export const Native = {
