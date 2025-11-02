e20fbd0372e86a9008aee63a4677212518ea2634
Get <getdna99@gmail.com>
2025-11-02 12:46:51 +0700

    fix: add other files for local storage migration

--- app/pages/notes/index.vue
+++ app/pages/notes/index.vue
@@ -2,15 +2,21 @@
 import { createNewNoteAsync, getNotesAsync, getNoteTitleAsync } from '~/utils/notes/manager';
 import { Apps } from '~/utils/pages/app';
 usePageInfo(Apps.pages.find(x => x.path === 'notes'))
+import { ref, onMounted } from 'vue'
+
+const keys = await getNotesAsync()
+const notes = await Promise.all(
+    keys.map(async (k) => ({ key: k, title: await getNoteTitleAsync(k) }))
+)
 </script>
 <template>
     <Feature category="snippets" tool="Quick Templates" kind="app" app="do-not-center"
         class="md:mx-12 my-12 flex flex-col shrink-0 gap-4">
         <AppSaveWarning />
         Recent Notes
-        <OurLink v-for="key in await getNotesAsync()" :key :href="`notes/${key}`" class="manual">
+        <OurLink v-for="{ title, key } in notes" :key :href="`./${key}`" class="manual">
             <Control>
-                <div class="text-card-title">{{ await getNoteTitleAsync(key) }}</div>
+                <div class="text-card-title">{{ title }}</div>
             </Control>
         </OurLink>
         <Button @click="async () => {
